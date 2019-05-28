03ccba282504976701e15445a381e16be12a2f4d
Eric de Graaf <eric.degraaf@alten.nl>
2019-05-28 15:27:30 +0200

    update provision file

--- provision.sh
+++ provision.sh
@@ -16,28 +16,39 @@ export APITEST_GIT_URL=$GIT_BASE_URL/$APITEST_NAME.git
 export GUITEST_GIT_URL=$GIT_BASE_URL/$GUITEST_NAME.git
 export DEPLOY_IDE=false
 
-echo  provisioning the Virtual machine
+echo provisioning the Virtual machine
 
 echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" | sudo tee /etc/apt/sources.list.d/webupd8team-java.list
 echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" | sudo tee -a /etc/apt/sources.list.d/webupd8team-java.list
 echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
 echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys EEA14886
-head -n -2 /etc/apt/sources.list > /etc/apt/sources.text ; cd /etc/apt/ ; mv sources.text sources.list
+
+echo  Remove original sources list and provide own
+rm /etc/apt/sources.list
+
+echo -e "deb http://http.debian.net/debian jessie main\n" \
+    "deb-src http://http.debian.net/debian jessie main\n" \
+    "deb http://security.debian.org/ jessie/updates main contrib\n" \
+    "deb-src http://security.debian.org/ jessie/updates main contrib" > /etc/apt/sources.list
+
 apt-get update
 apt-get install -y build-essential
 apt-get install -y libmysqlclient-dev
 apt-get install -y git
-apt-get install -y firefox-esr #browsers 
+apt-get install -y firefox-esr
 apt-get install -y oracle-java8-installer
 apt-get install -y oracle-java8-set-default
 apt-get install -y maven
 apt-get install -y libappindicator3-1 
 
-# Install SUT #
+
+echo @@@ Deploy SUT @@@
+cd $HOME
+# Install SUT from GIT #
 if [[ -d ./$SUT_NAME ]]; then
-	echo "Remove old dir in image. " + $SUT_NAME
-	rm -rf $SUT_NAME
+	echo "Remove old dir in image: " $SUT_NAME
+	rm -rf ./$SUT_NAME
 fi
 git clone $SUT_GIT_URL
 cd $SUT_NAME
